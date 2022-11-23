49208e52c9102c1812eef755d75cac1ca9dc0677
guozhanxin <guozhanxin@rt-thread.com>
2022-11-23 21:29:22 +0800

    sched can yield

--- arch/arm/cortex-m4/arch_thread.c
+++ arch/arm/cortex-m4/arch_thread.c
@@ -13,11 +13,11 @@ extern struct _thread_obj *_g_next_thread;
 
 uint32_t arch_switch_interrupt_flag;
 
-static struct arch_thread
+struct arch_thread
 {
     uint32_t basepri;
     uint32_t stack_ptr;
-}_arch_thread_data;
+};
 
 typedef struct exc_stack_frame
 {
@@ -31,30 +31,54 @@ typedef struct exc_stack_frame
     uint32_t psr;
 }_esf_t;
 
+typedef struct stack_frame
+{
+    /* r4 ~ r11 register */
+    uint32_t r4;
+    uint32_t r5;
+    uint32_t r6;
+    uint32_t r7;
+    uint32_t r8;
+    uint32_t r9;
+    uint32_t r10;
+    uint32_t r11;
+
+    _esf_t esf;
+}_sf_t;
 
 void arch_new_thread(struct _thread_obj *thread,
                             void    *stack_addr,
                             uint32_t stack_size)
 {
-    _esf_t *esf;
-    esf = (_esf_t *)P_ALIGN_DOWN(((uint32_t)stack_addr + stack_size) - sizeof(_esf_t), 8);
+    int i;
+    struct arch_thread *arch_data;
+    _sf_t *sf;
+    
+    arch_data = (struct arch_thread *)P_ALIGN_DOWN(((uint32_t)stack_addr + stack_size) - sizeof(struct arch_thread), 8);
+    sf = (_sf_t *)P_ALIGN_DOWN((uint32_t)arch_data - sizeof(_sf_t), 8);
 
-    esf->r0 = (uint32_t)thread->entry;
-    esf->r1 = (uint32_t)thread->param;
-    esf->r2 = 0;
-    esf->r3 = 0;
-    esf->r12 = 0;
-    esf->lr = 0;
-    esf->pc = (uint32_t)p_thread_entry;
-    esf->psr = 0x01000000UL;
+    /* init all register */
+    for (i = 0; i < sizeof(_sf_t) / sizeof(uint32_t); i ++)
+    {
+        ((uint32_t *)sf)[i] = 0xdeadbeef;
+    }
+    
+    sf->esf.r0 = (uint32_t)thread->entry;
+    sf->esf.r1 = (uint32_t)thread->param;
+    sf->esf.r2 = 0;
+    sf->esf.r3 = 0;
+    sf->esf.r12 = 0;
+    sf->esf.lr = 0;
+    sf->esf.pc = (uint32_t)p_thread_entry;
+    sf->esf.psr = 0x01000000UL;
 
-    _arch_thread_data.stack_ptr = ((uint32_t)esf);
-    thread->arch = &_arch_thread_data;
+    arch_data->stack_ptr = ((uint32_t)sf);
+    thread->arch = arch_data;
 }
 
 uint32_t arch_thread_to_ptr(struct _thread_obj *thread)
 {
-    return thread->arch->stack_ptr;
+    return (uint32_t)&thread->arch->stack_ptr;
 }
 
 void arch_swap(unsigned int key)
@@ -90,6 +114,7 @@ __attribute__((always_inline)) static inline void do_swap(void)
     __asm ("    LDR     r0, =_g_curr_thread");
     __asm ("    LDR     r1, [r0]");
     __asm ("    CBZ     r1, switch_to_thread ");   // skip register save at the first time
+    __asm ("    MOV     r0, r1");
 
     
     __asm ("    PUSH    {r0, lr}");
@@ -99,8 +124,6 @@ __attribute__((always_inline)) static inline void do_swap(void)
     __asm ("    MRS     r1, psp              ");   // get from thread stack pointer
 
     __asm ("    STMFD   r1!, {r4 - r11}      ");   // push r4 - r11 register
-
-    __asm ("    LDR     r0, [r0]");
     
     __asm ("    PUSH    {lr}");
     __asm ("    BL      arch_thread_to_ptr");
@@ -113,8 +136,11 @@ __attribute__((always_inline)) static inline void do_swap(void)
     __asm ("    PUSH    {lr}");
     __asm ("    BL      arch_thread_to_ptr");
     __asm ("    POP     {lr}");
-    __asm ("    MOV     r1, r0");
-
+    
+    __asm ("    LDR     r1, [r0]");
+    
+    __asm ("    LDMFD   r1!, {r4 - r11}");         // pop r4 - r11 register
+    
     __asm ("    MSR     psp, r1");//  update stack pointer
     
     __asm ("    PUSH    {r0, lr}");
