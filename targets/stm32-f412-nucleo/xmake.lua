6add24d8e776a87f053bd663c54a1730be588657
guozhanxin <guozhanxin@rt-thread.com>
2023-06-28 23:27:20 +0800

    update

--- targets/stm32-f412-nucleo/xmake.lua
+++ targets/stm32-f412-nucleo/xmake.lua
@@ -1,6 +1,6 @@
 
 
-target("hello")
+target("puppy")
     add_files("Core/Src/*.c", "/Drivers/**.c")
     add_defines("USE_HAL_DRIVER", "STM32F412Zx")
     add_includedirs("Drivers/STM32F4xx_HAL_Driver/Inc", 
@@ -20,9 +20,9 @@ target("hello")
         add_ldflags('--strict --scatter ' .. os.scriptdir() .. '/link.sct')
         after_build(function (package)
             if is_mode('debug') then
-                os.exec("D:/Progrem/Keil_v5/ARM/ARMCLANG/bin/fromelf.exe --bin $(buildir)/cross/cortex-m4/debug/hello.axf --output puppy.bin")
+                os.exec("D:/Progrem/Keil_v5/ARM/ARMCLANG/bin/fromelf.exe --bin $(buildir)/cross/cortex-m4/debug/puppy.axf --output puppy.bin")
             else
-                os.exec("D:/Progrem/Keil_v5/ARM/ARMCLANG/bin/fromelf.exe --bin $(buildir)/cross/cortex-m4/release/hello.axf --output puppy.bin")
+                os.exec("D:/Progrem/Keil_v5/ARM/ARMCLANG/bin/fromelf.exe --bin $(buildir)/cross/cortex-m4/release/puppy.axf --output puppy.bin")
             end
         end)
     elseif is_config("toolchian", "arm-none-eabi-gcc") then
@@ -34,9 +34,9 @@ target("hello")
         add_ldflags(' -T ' .. os.scriptdir() .. '/link.lds')
         after_build(function (package)
             if is_mode('debug') then
-                os.exec("D:/Progrem/env-windows/tools/gnu_gcc/arm_gcc/mingw/bin/arm-none-eabi-objcopy.exe -O binary $(buildir)/cross/cortex-m4/debug/hello.elf puppy.bin")
+                os.exec("D:/Progrem/env-windows/tools/gnu_gcc/arm_gcc/mingw/bin/arm-none-eabi-objcopy.exe -O binary $(buildir)/cross/cortex-m4/debug/puppy.elf puppy.bin")
             else
-                os.exec("D:/Progrem/env-windows/tools/gnu_gcc/arm_gcc/mingw/bin/arm-none-eabi-objcopy.exe -O binary $(buildir)/cross/cortex-m4/release/hello.elf puppy.bin")
+                os.exec("D:/Progrem/env-windows/tools/gnu_gcc/arm_gcc/mingw/bin/arm-none-eabi-objcopy.exe -O binary $(buildir)/cross/cortex-m4/release/puppy.elf puppy.bin")
             end
         end)       
     end
