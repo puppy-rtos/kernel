6f2db526e2a547f4b15f9b4984334224a63820d7
guozhanxin <guozhanxin@rt-thread.com>
2023-08-03 22:31:17 +0800

    更新工程

--- targets/qemu-virt-riscv/xmake.lua
+++ targets/qemu-virt-riscv/xmake.lua
@@ -7,19 +7,20 @@ if is_config("build_target", "qemu-virt-riscv") then
         "_POSIX_C_SOURCE=199309L"
     )
     add_files(
-        "*.c",
-        "*.S"
+        "**.c",
+        "**.S"
     )
     add_includedirs(
-        "."
+        "drivers",
+        "libraries"
     )
     set_toolchains("cross")
     set_extension(".elf")
     set_arch("riscv32")
     add_cxflags(' -march=rv32imac -mabi=ilp32 -mcmodel=medany -Wall -fmessage-length=0 -ffunction-sections -fdata-sections -fno-builtin-printf')
     add_asflags(' -c -march=rv32imac -mabi=ilp32 -mcmodel=medany -Wall -fmessage-length=0 -ffunction-sections -fdata-sections -fno-builtin-printf')
-    add_ldflags(' -march=rv32imac -mabi=ilp32 -mcmodel=medany -nostartfiles -Xlinker --gc-sections -Xlinker --defsym=__stack_size=300 -Xlinker -Map=RTOSDemo.map ')
-    add_ldflags(' -T ' .. os.scriptdir() .. '/fake_rom.lds')
+    add_ldflags(' -march=rv32imac -mabi=ilp32 -mcmodel=medany -nostartfiles -Xlinker --gc-sections -Xlinker --defsym=__stack_size=300 -Xlinker -Map=puppy.map ')
+    add_ldflags(' -T ' .. os.scriptdir() .. '/link.lds')
     after_build(function (package)
         if is_mode('debug') then
             os.exec("$(sdk)/bin/riscv64-unknown-elf-objcopy -O binary $(buildir)/cross/riscv32/debug/puppy.elf puppy.bin")
